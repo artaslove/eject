71258c7bf6a312c2911dd0a124f8fb5777e834ad
Tony <bonafide@martica.org>
2018-03-16 08:16:39 -0700

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This script installs eject.ahk, which requires AutoHotKey available from https://autohotkey.com/download/ ... 
+pause
+copy eject.ahk %systemroot%\
+REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v eject /t REG_EXPAND_SZ /d ^%systemroot^%\eject.ahk
+%systemroot%\eject.ahk
+echo done!
