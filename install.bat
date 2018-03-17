f68374ca266283181c63862ba649aca88075d54c
Tony <bonafide@martica.org>
2018-03-16 20:36:39 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,3 +5,4 @@ copy eject.ahk %systemroot%\
 REG ADD HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v eject /t REG_EXPAND_SZ /d ^%systemroot^%\eject.ahk
 %systemroot%\eject.ahk
 echo done!
+REM ToDo: a proper installer, that makes sure we are admin and everything is actually installed
